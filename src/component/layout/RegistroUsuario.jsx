e99b58fd05bfeabf39d34c9c75e4a590955465fa
montcristian <126423782+montcristian@users.noreply.github.com>
2024-03-24 16:55:28 -0500

    registro de usuario

--- src/component/layout/RegistroUsuario.jsx
+++ src/component/layout/RegistroUsuario.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
-const RegistroUsuario = ({ nombre, correo, contrasena, onChangeNombre, onChangeCorreo, onChangeContrasena, onSubmit }) => {
+import './RegistroUsuario.css'
+const RegistroUsuario = ({ nombre, correo, contrasena, edad, onChangeNombre, onChangeCorreo, onChangeEdad, onChangeContrasena, onSubmit }) => {
   return (
     <div className="registro-container">
       <form className="registro-form" onSubmit={onSubmit}>
-        <h2>Registro</h2>
+        <h2>Registro de usuario</h2>
         <input 
           value={nombre} 
           type="text" 
@@ -17,15 +17,21 @@ const RegistroUsuario = ({ nombre, correo, contrasena, onChangeNombre, onChangeC
           placeholder="Ingresa tu correo electrónico" 
           onChange={onChangeCorreo} 
         />
+           <input 
+          value={edad} 
+          type="number" 
+          placeholder="Ingresa tu edad" 
+          onChange={onChangeEdad} 
+        />
         <input 
           value={contrasena} 
           type="password" 
           placeholder="Ingresa tu contraseña" 
           onChange={onChangeContrasena} 
         />
+     
         <input type="submit" value="Registrarse" />
-        <div className="links">
-        </div>
+        
       </form>
     </div>
   );
