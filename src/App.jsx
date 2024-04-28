556150c63d11ab70a99473556d6508d0abdb7448
montcristian <126423782+montcristian@users.noreply.github.com>
2024-04-28 16:12:33 -0500

    back  y front

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import Login from "./component/layout/Login";
 import RegistroUsuario from './component/layout/RegistroUsuario';
-import Contenido from './component/layout/Contenido';
-import Reserva from './component/layout/Reserva';
 
 function App() {
   /// Funciones y estados para el inicio de sesión
@@ -18,13 +16,12 @@ function App() {
   }
 
   /// Funciones y estados para el registro de usuario
-  const [nombreRegistro, setNombreRegistro] = useState("");
+  const [nombre, setNombre] = useState("");
   const [correo, setCorreo] = useState("");
   const [contrasenaRegistro, setContrasenaRegistro] = useState("");
-  const [edad, setEdad] = useState("");
 
   const handleChangeNombre = (event) => {
-    setNombreRegistro(event.target.value);
+    setNombre(event.target.value);
   }
 
   const handleChangeCorreo = (event) => {
@@ -35,40 +32,10 @@ function App() {
     setContrasenaRegistro(event.target.value);
   }
 
-  const handleChangeEdad = (event) => {
-    setEdad(event.target.value);
-  }
-
   const handleSubmitRegistro = (event) => {
     event.preventDefault();
     // Aquí puedes realizar la lógica para enviar los datos del formulario de registro
   }
-
-  // Funciones y estados para la reserva 
-  const [nombreReserva, setNombreReserva] = useState('');
-  const [apellido, setApellido] = useState('');
-  const [email, setEmail] = useState('');
-  const [fecha, setFecha] = useState('');
-  const [hora, setHora] = useState('');
-  const [cantidad, setCantidad] = useState('');
-  const [aceptarTerminos, setAceptarTerminos] = useState(false);
-
-  const handleChangeAceptarTerminos = () => {
-    setAceptarTerminos(!aceptarTerminos);
-  };
-
-  // Función para manejar el envío del formulario de reserva
-  const handleSubmitReserva = (e) => {
-    e.preventDefault();
-    if (aceptarTerminos) {
-      // Aquí puedes enviar los datos del formulario a donde necesites
-      alert('Reserva exitosa');
-      // Puedes hacer alguna acción adicional aquí, como enviar los datos a un servidor
-      // Por ejemplo, puedes usar fetch() o axios para enviar los datos a un servidor
-    } else {
-      alert('Debes aceptar los términos y condiciones para realizar la reserva.');
-    }
-  };
 
   return (
     <>
@@ -80,38 +47,19 @@ function App() {
       />
 
       <RegistroUsuario
-        nombre={nombreRegistro}
+        nombre={nombre}
         correo={correo}
         contrasena={contrasenaRegistro}
-        edad={edad}
         onChangeNombre={handleChangeNombre}
         onChangeCorreo={handleChangeCorreo}
         onChangeContrasena={handleChangeContrasenaRegistro}
-        onChangeEdad={handleChangeEdad}
         onSubmit={handleSubmitRegistro}
-      />
 
-      <Contenido />
 
-      <Reserva
-        nombre={nombreReserva}
-        apellido={apellido}
-        email={email}
-        fecha={fecha}
-        hora={hora}
-        cantidad={cantidad}
-        setNombre={setNombreReserva}
-        setApellido={setApellido}
-        setEmail={setEmail}
-        setFecha={setFecha}
-        setHora={setHora}
-        setCantidad={setCantidad}
-        aceptarTerminos={aceptarTerminos}
-        onChangeAceptarTerminos={handleChangeAceptarTerminos}
-        handleSubmit={handleSubmitReserva}
+        
       />
     </>
-  );
+  )
 }
 
 export default App;
--- src/component/layout/RegistroUsuario.jsx
+++ src/component/layout/RegistroUsuario.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './RegistroUsuario.css'
-const RegistroUsuario = ({ nombre, correo, contrasena, edad, onChangeNombre, onChangeCorreo, onChangeEdad, onChangeContrasena, onSubmit }) => {
+
+const RegistroUsuario = ({ nombre, correo, contrasena, onChangeNombre, onChangeCorreo, onChangeContrasena, onSubmit }) => {
   return (
     <div className="registro-container">
       <form className="registro-form" onSubmit={onSubmit}>
-        <h2>Registro de usuario</h2>
+        <h2>Registro</h2>
         <input 
           value={nombre} 
           type="text" 
@@ -17,22 +17,15 @@ const RegistroUsuario = ({ nombre, correo, contrasena, edad, onChangeNombre, onC
           placeholder="Ingresa tu correo electrónico" 
           onChange={onChangeCorreo} 
         />
-           <input 
-          value={edad} 
-          type="number" 
-          placeholder="Ingresa tu edad" 
-          onChange={onChangeEdad} 
-        />
         <input 
           value={contrasena} 
           type="password" 
           placeholder="Ingresa tu contraseña" 
           onChange={onChangeContrasena} 
         />
-     
         <input type="submit" value="Registrarse" />
-        
-        <input type="submit" value="cancelar" />
+        <div className="links">
+        </div>
       </form>
     </div>
   );
