306db3a220d2d6bdcb8e51f64718d7ae3ef0b345
montcristian <126423782+montcristian@users.noreply.github.com>
2024-06-08 14:54:55 -0500

    editarusuarios

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Contenido from "./component/layout/pages/Contenido.jsx";
 import RegistroUsuario from "./component/layout/auth/RegistroUsuario.jsx";
 import ReservaUsuario from "./component/layout/users/ReservaUsuario.jsx"
 import Reserva from "./component/layout/auth/Reserva.jsx";
+import EditarReserva from "./component/layout/users/EditarReserva.jsx";
+
 function App() {
 
   
@@ -21,6 +23,8 @@ function App() {
       <Reserva/>
      
       <ReservaUsuario/>
+
+      <EditarReserva/>
 
     </>
   );
